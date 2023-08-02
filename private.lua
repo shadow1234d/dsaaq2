2e59f9fa2b576f5fe058fc819b7f04839aa0a25a
shadow1234d <116893153+shadow1234d@users.noreply.github.com>
2023-08-02 12:41:14 +0300

    Update private.lua

--- private.lua
+++ private.lua
@@ -1,1 +1,30 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
+local Window = OrionLib:MakeWindow({Name = "Private Script", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local scr = Window:MakeTab({
+	Name = "Owned Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+scr:AddButton({
+	Name = "Key System",
+	Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/shadow1234d/dsaaq2/main/main.lua')))()
+  	end    
+})
+
+scr:AddButton({
+	Name = "Hacker's GUI",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/shadow1234d/dsaaq2/main/script.lua"))()
+  	end    
+})
+
+scr:AddButton({
+	Name = "Old Script",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/s0shadow0BG/ShadowHack1542/main/hack.lua'))()
+  	end    
+})
